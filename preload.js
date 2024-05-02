a4f24883b0ee7f6d7458184f61099f421541c474
Tyler Barnhill <58178655+tbarnhill@users.noreply.github.com>
2024-05-02 15:43:48 -0500

    Update to electron v30.0.2

--- preload.js
+++ preload.js
@@ -1,12 +1,6 @@
-// All of the Node.js APIs are available in the preload process.
-// It has the same sandbox as a Chrome extension.
-window.addEventListener('DOMContentLoaded', () => {
-  const replaceText = (selector, text) => {
-    const element = document.getElementById(selector)
-    if (element) element.innerText = text
-  }
-
-  for (const type of ['chrome', 'node', 'electron']) {
-    replaceText(`${type}-version`, process.versions[type])
-  }
+const { contextBridge } = require('electron')
+let fs = require('fs-extra')
+console.log('preload script')
+contextBridge.exposeInMainWorld('imports', {
+    fs:fs 
 })
